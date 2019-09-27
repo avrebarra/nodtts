353001539da21c9242ed25b316444dc8bd7bc818
Avreghly Barra <avreghly@gmail.com>
2019-09-28 06:09:11 +0700

    Finish base API

--- index.js
+++ index.js
@@ -1,11 +1,58 @@
+import https from 'https'
+import fs from 'fs'
 import tts from 'google-tts-api'
 
-export const gen = async (text, language = 'en', speed = 1.0) => {
-  if (!text) throw new Error('Please supply text to convert!')
+/**
+ * Get speech url from provided text
+ *
+ * @param  {string} text text to translate
+ * @param  {string} lang='en' locale to use when generating speech file
+ * @param  {number} speed=1.0 speech speed
+ */
+export const url = async (text, lang = 'en', speed = 1.0) => {
+  if (!text) throw new Error('Supply text to convert!')
 
-  const url = await tts(text, language, speed)
-
-  return url
+  return tts(text, lang, speed)
 }
 
-export default { gen }
+/**
+ * Get speech download stream from provided text
+ *
+ * @param  {string} text text to translate
+ * @param  {string} lang='en' locale to use when generating speech file
+ * @param  {number} speed=1.0 speech speed
+ */
+export const stream = async (text, lang, speed) => {
+  const resurl = await url(text, lang, speed)
+
+  return new Promise((resolve, reject) => {
+    https.get(resurl, (response) => {
+      resolve(response)
+    })
+  })
+}
+
+/**
+ * Download speech to file from provided text
+ *
+ * @param  {string} path path to save speech file
+ * @param  {string} text text to translate
+ * @param  {string} lang='en' locale to use when generating speech file
+ * @param  {number} speed=1.0 speech speed
+ */
+export const file = async (path, text, lang, speed) => {
+  if (!path) throw new Error('Supply path to save file to!')
+
+  const resstream = await stream(text, lang, speed)
+  const fwstream = fs.createWriteStream(path)
+
+  resstream.pipe(fwstream)
+
+  return new Promise((resolve, reject) => {
+    fwstream.on('finish', function () {
+      fwstream.close(resolve)
+    })
+  })
+}
+
+export default { url, stream, file }
